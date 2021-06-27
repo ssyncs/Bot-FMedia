4587adfa7f225f9153a1b277f5bcee3c9048af13
ssyncs <86572006+ssyncs@users.noreply.github.com>
2021-06-27 21:23:21 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    "TOKEN:" : "ODU4Nzg0MzU5NTM2Mzk0Mjcx.YNjLQg.3n51BdkgCkIibDFwjO6XDCXCmQE",
+    "TOKEN:" : "ODU4Nzg0MzU5NTM2Mzk0Mjcx.YNjLQg.wGYr2_oooL4uOFTgH9l2aVF1btU",
     "PREFIX" : "!",
 
 
